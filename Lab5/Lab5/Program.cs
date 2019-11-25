94f0aa3eec3833c2aa6224a059bf3da673a8bf0a
hyperionx13 <hyperionx13@gmail.com>
2019-11-24 20:58:15 -0500

    commit 1

---
+++ Lab5/Lab5/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Lab5
+{
+    /* class Program
+     {
+         public double getArea(double height, double width)
+         {
+             return height * width;
+
+              A. Scope - double height, double width
+              * b. static vs. non-static - static information can be accessed in the whole code.
+              * c. return type - double
+              * d. method name (identifier) - getArea
+              * e. parameters - public
+              * f. method body - return height * width; this is the part that will be exicuted
+
+
+         }
+     }*/
+
+    /* 2. a user defined method is one that is created by the user, where a method that is provided
+    with a frame work are predefined and when creating user defined method you should try and find
+    a predefined alternatives 
+
+     3. a non-static method can acess all non-static feilds in object and the static method
+     can not acess any objects in non-static feilds
+     */
+
+    /*public static void bark();
+        {
+            Console.WriteLine("{0} is barking...", Name);
+
+        }
+        public static void doTrick(trickName);
+    {
+    Console.WriteLine("{0} is so smart!  {0} is doing a(n) {1}", name, trickName);
+    }
+    
+     I think that this would be the answers i just am unable to open the code in the files you proveided
+     
+     */
+}
