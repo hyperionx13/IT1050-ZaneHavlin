2009b7b50f68b807262911140c569b5cc257a131
hyperionx13 <hyperionx13@gmail.com>
2019-12-01 20:43:47 -0500

    first

---
+++ Lab6/Lab6/Program.cs
@@ -0,0 +1,99 @@
+﻿using System;
+
+namespace Lab6
+{
+    class Program
+    {
+        /*2. A one-dimensional array p contains four elements.  
+           The array access expressions to access each of the elements in p are 
+                               0, 1, 2, and 3      */
+        static void Main(string[] args)
+        {//3.
+
+           /* int[] array = new int[12];
+
+            Console.WriteLine("{0}{1,15}", "index, month");
+            {
+                string[] month = new string[12];
+                month[0] = "January";
+                month[1] = "February";
+                month[2] = "March";
+                month[3] = "April";
+                month[4] = "May";
+                month[5] = "June";
+                month[6] = "July";
+                month[7] = "August";
+                month[8] = "September";
+                month[9] = "October";
+                month[10] = "November";
+                month[11] = "December";
+                for (int counter = 0; counter < array.Length; ++counter) ;
+                {
+                    Console.WriteLine("{o}{1,15}", counter, string[month]);
+                }
+            }*/
+
+            /*{//4.
+                string[] seasons = { "winter", "spring", "Summer", "Fall" };
+                Console.WriteLine("the four seasons");
+                foreach (string season in seasons)
+                {
+                    Console.WriteLine(season);
+                }
+            }*/
+
+            /*{//5.
+                int[] array;
+                array = new int[1000];
+                {
+                Random randomnumbers = new Random(1000);
+                int randomNumber = randomNumber.Next();
+                    {
+                        foreach (randomNumber in randomNumber.Next(0, 100)) ;
+                        Console.WriteLine("{0}");
+                    }
+
+                }*/
+            /*{//6. 
+
+                string[] names = { "Al Dente", "Anna Graham", "Earle Bird", "Ginger Rayle", "Iona Ford" };
+
+                int i = 0;
+                {
+                    while (i < names.Length)
+
+                        Console.WriteLine(string[names]);
+
+                }
+
+            }*/
+
+            /*{//7.
+
+                  string[] names = new string[5] { "Al Dente", "Anna Graham", "Earle Bird", "Ginger Rayle", "Iona Ford" };
+                  int i = 0;
+                  {
+                      for (int i = 0; i < names.Length; ++i) ;
+
+                      Console.WriteLine("{0,2}. {1}", i, names[i]);
+                  }
+
+
+              }*/
+
+            /*{//8.
+
+                string[] names = new string[5] { "Al Dente", "Anna Graham", "Earle Bird", "Ginger Rayle", "Iona Ford" };
+                int i = 0;
+                {
+                    foreach (string name in names)
+
+                        Console.WriteLine("{0,2}. {1}", i, names[i]);
+                }
+
+
+            }*/
+
+        }
+    }
+}
---
+++ midterm/midterm/Program.cs
@@ -0,0 +1,113 @@
+﻿using System;
+
+namespace midterm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // loops - loops will repeat a portion of code undill the termination requirements are met. 
+            // if statments - both if and if-else when proven ture or false chose the propper code to output
+            //switch statments - a switch statments can replace a if-else statment. 
+
+            /*{
+                int i = 20;
+                Boolean keepLooping = true;
+                while(keepLooping)
+                {
+                    if (i <= 19)
+                        keepLooping = false;
+                    i++;
+                    Console.WriteLine(i);
+                }
+            }*/
+            /*  int i = 2;
+              while (i <=128)
+              {
+                  {
+                      if ((i % 2) == 0)
+                      {
+                          Console.WriteLine("[{0}]", i);
+                      }
+                      else if ((i % 2) != 0)
+                          {
+                              Console.Write("");
+                          }
+                  i *= 2;
+                  }
+              }*/
+
+            /*    string j = ",";
+                int i;
+                for (i = 49; i >= 1; --i)
+                {
+                    Console.Write(i);
+                    if (i >= 2)
+                    {
+                        Console.Write(j);
+                    }
+
+                }*/
+
+            /*  int i = 1;
+              while (i <= 21)
+              {
+                  {
+                      if ((i % 2) == 0)
+                      {
+                          Console.Write(" ");
+                      }
+                      else if ((i % 2) != 0)
+                      {
+                          Console.Write(i);
+                      }
+                      i++;
+                  }
+              }*/
+
+            /* int n = 8;
+             int i = 10; // initialize
+             do
+             {
+                 Console.Write("*");
+                 i++; // update!
+             } while (i < n); // test condition
+             // the output is *
+             */
+
+            /* int n = 2;
+             int i = 1;
+             while (i < n)
+             {
+                 Console.Write("*");
+                 i++;   
+             }*/
+
+
+            //we combine multiple boolean values by nested if else or if statments.
+            /*{
+                bool icyrain = false, tornadowarning = false;
+                if (icyrain == false && tornadowarning == false)
+                {
+                    Console.WriteLine("Lets go outside");
+                }
+
+              }*/
+
+            int numberoflayer = 5, Space, Number;
+            for (int i = 1; i <= numberoflayer; i++)
+            {
+                for (Space = 1; Space <= (numberoflayer - i); Space++)
+                    Console.Write("");
+                for (Number = 1; Number <= i; Number++)
+                    Console.Write(Number);
+                for (Number = (i - 1); Number >= 1; Number--)
+                    Console.Write(Number);
+                Console.WriteLine();
+                //it writes upside down and on half a pyramid, what did i do wrong. i feel like is something easy but i can not figure it out.
+
+
+            }
+        }
+    }
+}
